7619b429e93554b437aede528e8a1eccc772870f
Aminul Islam <aminulislam681538@gmail.com>
2022-02-12 20:17:43 +0600

    All div (bio,skills,links) are organized are into three separated files and import the files into index files

---
+++ src/components/profile/Bio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Bio = () => (
+  <div className="bio">
+    <h3>Aminul Islam</h3>
+    <p>FullStack JavaScript Developer</p>
+  </div>
+);
+
+export default Bio;
--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,40 +1,10 @@
 import React from "react";
 import "./style.css";
 
-const Bio = () => (
-  <div className="bio">
-    <h3>Aminul Islam</h3>
-    <p>FullStack JavaScript Developer</p>
-  </div>
-);
+import Bio from "./Bio";
+import Skills from "./Skills";
+import SocialsLink from "./Links";
 
-const Skills = () => (
-  <div className="skills">
-    <h3>Skills: </h3>
-    <ul>
-      <li>JavaScript</li>
-      <li>C/C++</li>
-      <li>Java</li>
-    </ul>
-  </div>
-);
-
-const SocialsLink = () => (
-  <div className="socials-links">
-    <h3>Socials links :</h3>
-    <ul>
-      <li>
-        <a href="#">FaceBook</a>
-      </li>
-      <li>
-        <a href="#">linkedIn</a>
-      </li>
-      <li>
-        <a href="#">Github</a>
-      </li>
-    </ul>
-  </div>
-);
 class Profile extends React.Component {
   render() {
     return (
