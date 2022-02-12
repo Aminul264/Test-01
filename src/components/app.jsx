3ccb2d79f3e95c194f7baa82197ba14a5b25ced8
Aminul Islam <aminulislam681538@gmail.com>
2022-02-12 23:59:08 +0600

    this is nothing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,19 @@
 import React,{Component} from "react";
 
 import Profile from './profile/index'
+//import Skills from "./profile/Skills";
+import MyProps from './props';
 
 class App extends Component{
     render(){
         return(
-            <div>
+            <div className="App">
                 <Profile/>
+                <div style ={{margin:'50px 35%'}}>
+                    <h3>Lists of programmers of my Company</h3>
+                    <MyProps name='Aminul Islam'/>
+                    <MyProps name='Nasim'/>
+                </div>
             </div>
         )
     }
---
+++ src/components/props/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * @propos er modde data ase,,,,koi theke ase>>>jekhan theke call depa hoy...sekhan theke ase attrbute  akare ase
+ */
+import React from "react";
+
+function MyProps(props){
+    //console.log('Props - ',props)
+    return <h1>My name is {props.name}</h1>;
+};
+
+export default MyProps;
